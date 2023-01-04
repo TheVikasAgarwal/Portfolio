889d67aefc8d9f282a69b52ab9ec32a482049f17
Salil Gupta <g.salil@live.in>
2023-01-04 22:52:38 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.14bd06869b2f47013ab615ac2ecf8a90.js"
+  "/Portfolio/precache-manifest.bd632b3d8a5aeef55f45b98db2abc888.js"
 );
 
 self.addEventListener('message', (event) => {
