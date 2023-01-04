7297ea9e8f82b665c735678c2723224e0757085d
Salil Gupta <g.salil@live.in>
2023-01-04 19:30:29 +0530

    Configure React app for deployment to GitHub Pages

---
+++ build/precache-manifest.f603a92ea9230b9817a18b4f71e886e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4de738965b8aa54461100d00078132a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "da5880a1a97e9cca446a",
+    "url": "/Portfolio/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "7b3c93d65f37bfc5a4f6",
+    "url": "/Portfolio/static/css/main.3f5021e6.chunk.css"
+  },
+  {
+    "revision": "da5880a1a97e9cca446a",
+    "url": "/Portfolio/static/js/2.bc1dca34.chunk.js"
+  },
+  {
+    "revision": "f55467d053b58e1408ff8a90dcc93571",
+    "url": "/Portfolio/static/js/2.bc1dca34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3c93d65f37bfc5a4f6",
+    "url": "/Portfolio/static/js/main.5b37d3ef.chunk.js"
+  },
+  {
+    "revision": "5fcc970351504b33245c",
+    "url": "/Portfolio/static/js/runtime-main.299e8944.js"
+  }
+]);
