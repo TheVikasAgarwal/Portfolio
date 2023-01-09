27bb12603d8fce972db59be12f6e995375a7aa00
Salil Gupta <g.salil@live.in>
2023-01-09 14:28:37 +0530

    Updates

--- precache-manifest.22e4cc32262fdadd03fae4c67a9ed197.js
+++ precache-manifest.22e4cc32262fdadd03fae4c67a9ed197.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e78244fa5c416d7d751b549148e2dbc2",
+    "revision": "822bd77a646370979e0520ae44998af6",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "da5880a1a97e9cca446a",
+    "revision": "226649df421552af4173",
     "url": "/Portfolio/static/css/2.80f479d8.chunk.css"
   },
   {
-    "revision": "d3ba4bfde4aa04500a5b",
+    "revision": "d262751d8224c4b69b0e",
     "url": "/Portfolio/static/css/main.3f5021e6.chunk.css"
   },
   {
-    "revision": "da5880a1a97e9cca446a",
-    "url": "/Portfolio/static/js/2.bc1dca34.chunk.js"
+    "revision": "226649df421552af4173",
+    "url": "/Portfolio/static/js/2.024696eb.chunk.js"
   },
   {
     "revision": "f55467d053b58e1408ff8a90dcc93571",
-    "url": "/Portfolio/static/js/2.bc1dca34.chunk.js.LICENSE.txt"
+    "url": "/Portfolio/static/js/2.024696eb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d3ba4bfde4aa04500a5b",
-    "url": "/Portfolio/static/js/main.026af89b.chunk.js"
+    "revision": "d262751d8224c4b69b0e",
+    "url": "/Portfolio/static/js/main.6763d731.chunk.js"
   },
   {
     "revision": "5fcc970351504b33245c",
